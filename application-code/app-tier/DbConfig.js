4fd0b7ecfb553079bc51a0d7eb7c4b701891b02d
Swapnil <swapnilsagar9111@gmail.com>
2024-07-30 09:58:02 +0530

    1st commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-c1444ggy8i6p.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Swapnil-1234',
+    DB_DATABASE : 'webappdb'
+});
